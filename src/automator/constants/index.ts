03b5d6fe33a702df3fabde8bf52c53cfae568889
wadakenji <ponporopen@gmail.com>
2024-07-30 16:53:33 +0900

    refactor: 環境変数を型キャスト

--- src/automator/constants/index.ts
+++ src/automator/constants/index.ts
@@ -1,2 +1,3 @@
-export const NATURE_REMO_CLOUD_API_TOKEN = process.env.NATURE_REMO_CLOUD_API_TOKEN
-export const MY_DEVICE_ID = process.env.MY_DEVICE_ID
+export const NATURE_REMO_CLOUD_API_TOKEN = process.env
+  .NATURE_REMO_CLOUD_API_TOKEN as string;
+export const MY_DEVICE_ID = process.env.MY_DEVICE_ID as string;
