17a2b4fb9e65c92815b4da883643d96468062049
wadakenji <ponporopen@gmail.com>
2024-08-01 19:27:03 +0900

    feat: グラフ表示

--- src/react/App.tsx
+++ src/react/App.tsx
@@ -1,7 +1,42 @@
+import { TemperatureRecord } from '../types';
+import { CartesianGrid, Line, LineChart, XAxis, YAxis } from 'recharts';
+import { format } from 'date-fns';
+
+//prettier-ignore
+const json: { datetime: string; temperature: number }[] = [];
+const data: TemperatureRecord[] = json.map(({ datetime, temperature }) => ({
+  temperature,
+  datetime: new Date(datetime),
+}));
+
 function App() {
   return (
     <main className="p-8">
-      <h1 className="font-bold text-2xl text-center">Vite + React</h1>
+      <LineChart
+        width={800}
+        height={500}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid />
+        <XAxis
+          dataKey="datetime"
+          tickFormatter={(value) => format(value, 'h:mm')}
+        />
+        <YAxis
+          dataKey="temperature"
+          domain={[25, 32]}
+          tickCount={8}
+          tickLine={false}
+          unit={'℃'}
+        />
+        <Line type="basis" stroke="#8884d8" dataKey="temperature" dot={false} />
+      </LineChart>
     </main>
   );
 }
