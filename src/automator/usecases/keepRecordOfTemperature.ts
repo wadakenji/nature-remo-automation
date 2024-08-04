0dcf5c77bc418d97977786138efb516ce0df6b66
wadakenji <ponporopen@gmail.com>
2024-08-04 16:51:38 +0900

    feat: エアコンの設定温度を記録

--- src/automator/usecases/keepRecordOfTemperature.ts
+++ src/automator/usecases/keepRecordOfTemperature.ts
@@ -2,6 +2,7 @@ import { addDays, format, getHours, startOfDay } from 'date-fns';
 import { getTemperature, getMyDevice } from '../nature-remo';
 import { readJsonFile, writeJsonFile } from '../../utils/json.ts';
 import { TemperatureRecord } from '../../types';
+import { getAirConditionerTemperatureSetting } from '../nature-remo/services/getAirConditionerTemperatureSetting.ts';
 
 export const keepRecordOfTemperature = () => {
   const executingDatetime = new Date();
@@ -16,16 +17,17 @@ export const keepRecordOfTemperature = () => {
     async () => {
       const myDevice = await getMyDevice();
       const temperature = await getTemperature(myDevice);
+      const airConditionerTemperatureSetting =
+        await getAirConditionerTemperatureSetting();
       const temperatureRecord: TemperatureRecord = {
         datetime: new Date(),
         temperature,
+        airConditionerTemperatureSetting,
       };
 
       const temperatureRecords = readJsonFile(filename) as TemperatureRecord[];
       temperatureRecords.push(temperatureRecord);
       writeJsonFile(filename, temperatureRecords);
-
-      console.log('write temperature.');
     },
     1000 * 60 * 5,
   );
